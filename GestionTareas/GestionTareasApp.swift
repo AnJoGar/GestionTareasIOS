ee3484c61d8e52cf9bd9ca2b42f170bda8abe059
aajordan <aajordan@netlife.net.ec>
2026-04-14 08:18:37 -0500

    Initial Commit

---
+++ GestionTareas/GestionTareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionTareasApp.swift
+//  GestionTareas
+//
+//  Created by Anderson Jordan Garcia on 14/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GestionTareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
